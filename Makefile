6fbe585b164d48ae3e177c362c7d8262490518bf
nodari-dev <nodari.pylypyshak@gmail.com>
2024-05-30 23:19:03 +0200

    whatever

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 build:
-	cc src/request_queue.c src/thread_pool.c src/request_reader.c src/http_parser.c src/main.c -o bin/server
+	cc src/request_queue.c src/request_reader.c src/http_parser.c src/main.c -o bin/server
 run:
 	./bin/server
 
 fun:
-	cc src/http_parser.c src/request_queue.c  src/thread_pool.c src/playground.c -o bin/playground
+	cc src/http_parser.c src/request_reader.c src/request_queue.c  src/playground.c -o bin/playground
 	./bin/playground
 
 
--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <pthread.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/socket.h>
+#include <time.h>
 #include <unistd.h>
 
 #include "../include/conf.h"
@@ -11,50 +13,41 @@
 #include "../include/http_parser.h"
 #include "../include/request_queue.h"
 #include "../include/request_reader.h"
-#include "../include/thread_pool.h"
+
+void *worker_thread(void *arg);
+void *request_thread(void *arg);
 
 int main() {
-  struct sockaddr_in host_address;
-  host_address.sin_family = AF_INET;
-  host_address.sin_port = htons(PORT);
-  host_address.sin_addr.s_addr = inet_addr(DEFAULT_HOST);
-
-  int server_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_socket_fd == FAILED) {
-    perror("Couldn't create a socket\n");
-    return 1;
-  }
-
-  if (bind(server_socket_fd, (struct sockaddr *)&host_address,
-           sizeof(host_address)) == FAILED) {
-    perror("Couldn't bind a socket\n");
-    return 1;
-  }
-
-  if (listen(server_socket_fd, SOMAXCONN) == FAILED) {
-    perror("this shit ain't gonna listen\n");
-    return 1;
-  }
-
   Request_Queue *request_queue = createQueue();
-  Thread_Pool *thread_pool = init_thread_pool(request_queue);
 
-  printf("Server is running on host: %s\n", DEFAULT_HOST);
-  int counter = 1;
+  pthread_t thread;
+  pthread_create(&thread, NULL, &request_thread, request_queue);
 
-  while (1) {
+  pthread_t worker_thread_arr[MAX_T];
+  for (int i = 0; i < MAX_T; ++i) {
+    pthread_create(&worker_thread_arr[i], NULL, worker_thread, request_queue);
+  }
 
-    struct sockaddr_in client_address;
-    socklen_t client_address_len = sizeof(host_address);
+  pthread_join(thread, NULL);
+  for (int i = 0; i < MAX_T; ++i) {
+    pthread_join(worker_thread_arr[i], NULL);
+  }
 
-    int client_socket_fd =
-        accept(server_socket_fd, (struct sockaddr *)&client_address,
-               &client_address_len);
+  return 0;
+}
 
-    if (client_socket_fd == FAILED) {
-      perror("Failed connecting to client\n");
-      continue;
+void *worker_thread(void *arg) {
+  Request_Queue *q = (Request_Queue *)arg;
+
+  for (;;) {
+    pthread_mutex_lock(&q->condition_mutex);
+    while (q->head == NULL) {
+      pthread_cond_wait(&q->not_empty, &q->condition_mutex);
     }
+    pthread_mutex_unlock(&q->condition_mutex);
+
+    pthread_mutex_lock(&q->mutex);
+    int client_socket_fd = deque(q);
 
     char *buffer = read_request(client_socket_fd);
     if (buffer == NULL) {
@@ -63,9 +56,7 @@ int main() {
 
     HTTP_REQUEST *http_request = parse_http_request(buffer);
     if (http_request != NULL) {
-      printf("%s\n", http_request->uri);
-      //    enque(request_queue, http_request);
-      // monitor_queue(request_queue);
+      printf("%sThread is working on s\n", http_request->uri);
     } else {
       printf("%s\n", "parsing error");
     }
@@ -75,27 +66,71 @@ int main() {
     buffer = NULL;
 
     char response[2048];
-    sprintf(response,
-            "HTTP/1.1 200 OK\r\n"
-            "Content-Type: text/html\r\n"
-            "Content-Length: 26\r\n"
-            "\r\n"
-            "<h1>%d</h1>",
-            counter);
-
-    counter++;
+    sprintf(response, "HTTP/1.1 200 OK\r\n"
+                      "Content-Type: text/html\r\n"
+                      "Content-Length: 26\r\n"
+                      "\r\n"
+                      "<h1>you suck</h1>");
 
     int write_res = write(client_socket_fd, response, sizeof(response));
-    if (write_res == FAILED) {
+    if (write_res == -1) {
       printf("Message was not sent\n");
     }
 
     free_http_request(http_request);
     http_request = NULL;
     close(client_socket_fd);
+    pthread_mutex_unlock(&q->mutex);
+  }
+}
+
+void *request_thread(void *arg) {
+  struct sockaddr_in host_address;
+  host_address.sin_family = AF_INET;
+  host_address.sin_port = htons(PORT);
+  host_address.sin_addr.s_addr = inet_addr(DEFAULT_HOST);
+
+  int server_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_socket_fd == -1) {
+    perror("Couldn't create a socket\n");
+  }
+
+  if (bind(server_socket_fd, (struct sockaddr *)&host_address,
+           sizeof(host_address)) == -1) {
+    perror("Couldn't bind a socket\n");
+  }
+
+  if (listen(server_socket_fd, SOMAXCONN) == -1) {
+    perror("this shit ain't gonna listen\n");
+  }
+
+  Request_Queue *q = (Request_Queue *)arg;
+
+  for (;;) {
+    struct sockaddr_in client_address;
+    socklen_t client_address_len = sizeof(host_address);
+
+    int client_socket_fd =
+        accept(server_socket_fd, (struct sockaddr *)&client_address,
+               &client_address_len);
+
+    if (client_socket_fd == -1) {
+      perror("Failed connecting to client\n");
+      continue;
+    }
+
+    pthread_mutex_lock(&q->mutex);
+    enque(q, client_socket_fd);
+    printf("New request: %d\n", client_socket_fd);
+    pthread_mutex_unlock(&q->mutex);
+
+    pthread_mutex_lock(&q->condition_mutex);
+    if (q->head != NULL) {
+      pthread_cond_signal(&q->not_empty);
+    }
+    pthread_mutex_unlock(&q->condition_mutex);
   }
 
   close(server_socket_fd);
-
-  return 0;
 }
+
